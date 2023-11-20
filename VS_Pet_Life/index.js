394a2a7a5c532195e40116883c136d2e1e3f33a1
Lucas Martins <138935675+martinssslucass@users.noreply.github.com>
2023-11-19 22:07:46 -0300

    Delete VS_Pet_Life directory

--- VS_Pet_Life/index.js
+++
@@ -1,7 +0,0 @@
-let elementosDuvida = document.querySelectorAll('.duvidas');
-
-elementosDuvida.forEach(function (duvida) {
-    // duvida.addEventListener('click', function (dasdsa) {
-
-    // }
-})
