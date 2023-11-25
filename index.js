c5d11a73364d5e91268e600632240962a270a5fc
Lucas Martins <138935675+martinssslucass@users.noreply.github.com>
2023-11-25 18:47:59 -0300

    Update index.js
    Update on js

--- index.js
+++ index.js
@@ -5,3 +5,28 @@ elementosDuvida.forEach(function (duvida) {
         duvida.classList.toggle('ativa')
     })
 })
+
+// mostrar / esconder o botão "Subir ao Topo"
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    var scrollToTopBtn = document.getElementById("subir-topo");
+
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+// Rolagem suave até o topo
+document.getElementById("subir-topo").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.body.scrollTop = 0; // Para navegadores mais antigos
+    document.documentElement.scrollTop = 0; // Para navegadores modernos
+});
